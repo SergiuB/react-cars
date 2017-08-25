9e2228efaaa878aecd12347ebc1e0c7f903cafcd
Sergiu Buciuc <buciuc_sergiu@yahoo.com>
2017-08-25 23:15:26 +0300

    CarForm

---
+++ src/components/CarForm/CarForm.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CarFormPresentation from './CarFormPresentation';
+
+class CarForm extends Component {
+  constructor(props) {
+    super(props);
+    const {
+      name,
+      acceleration
+    } = props;
+
+    this.state = {
+      name,
+      acceleration
+    };
+  }
+
+  changeName = (event) => {
+    const name = event.target.value;
+    const nameError = this.validateName(name);
+    this.setState({ name, nameError });
+  }
+
+  validateName = (name) => {
+    if (!name.length) {
+      return 'Name is mandatory';
+    }
+    return '';
+  }
+
+  changeAcceleration = (event) => {
+    const acceleration = parseInt(event.target.value, 10);
+    const accelerationError = this.validateNumber(acceleration, 1, 100);
+    this.setState({ acceleration, accelerationError });
+  }
+
+  validateNumber = (n, min, max) => {
+    if (isNaN(n) || !isFinite(n)) {
+      return 'Invalid number';
+    }
+    if (n < min) {
+      return `Value must be greater than ${min}`;
+    }
+    if (n > max) {
+      return `Value must be lower than ${max + 1}`;
+    }
+    return '';
+  }
+
+  submit = (event) => {
+    const {
+      name,
+      acceleration,
+    } = this.state;
+
+    this.props.onSubmit({ name, acceleration });
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <CarFormPresentation
+        {...this.state}
+        onChangeName={this.changeName}
+        onChangeAcceleration={this.changeAcceleration}
+        onSubmit={this.submit}
+        onCancel={this.props.onCancel}
+      />
+    );
+  }
+}
+
+CarForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  acceleration: PropTypes.number.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
+
+CarForm.defaultProps = {
+  onSubmit: () => {},
+  onCancel: () => {}
+};
+
+export default CarForm;
---
+++ src/components/CarForm/CarForm.test.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import CarForm from './CarForm';
+
+describe('CarForm', function () {
+  it('renders correctly', function () {
+    const wrapper = shallow(
+      <CarForm
+        name='Audi'
+        acceleration={12}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('updates props in response to change handlers', function () {
+    const wrapper = shallow(
+      <CarForm
+        name='Audi'
+        acceleration={12}
+      />
+    );
+
+    wrapper.prop('onChangeAcceleration')({ target: { value: 3 } });
+    expect(wrapper.prop('acceleration')).toEqual(3);
+
+    wrapper.prop('onChangeName')({ target: { value: 'VW' } });
+    expect(wrapper.prop('name')).toEqual('VW');
+  });
+
+  it('updates error props in response to setting invalid values', function () {
+    const wrapper = shallow(
+      <CarForm
+        name='Audi'
+        acceleration={12}
+      />
+    );
+
+    wrapper.prop('onChangeAcceleration')({ target: { value: NaN } });
+    expect(wrapper.prop('accelerationError')).toEqual('Invalid number');
+
+    wrapper.prop('onChangeName')({ target: { value: '' } });
+    expect(wrapper.prop('nameError')).toEqual('Name is mandatory');
+  });
+
+  it('calls onSubmit with the changed values', function () {
+    const onSubmit = jest.fn();
+    const wrapper = shallow(
+      <CarForm
+        name='Audi'
+        acceleration={12}
+        onSubmit={onSubmit}
+      />
+    );
+
+    wrapper.prop('onChangeAcceleration')({ target: { value: 10 } });
+    wrapper.prop('onChangeName')({ target: { value: 'VW' } });
+
+    wrapper.prop('onSubmit')({ preventDefault() {} });
+
+    expect(onSubmit).toBeCalledWith({ acceleration: 10, name: 'VW' });
+  });
+
+  it('calls onCancel', function () {
+    const onCancel = jest.fn();
+    const wrapper = shallow(
+      <CarForm
+        name='Audi'
+        acceleration={12}
+        onCancel={onCancel}
+      />
+    );
+
+    wrapper.prop('onCancel')();
+    expect(onCancel).toBeCalled();
+  });
+});
